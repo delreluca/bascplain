5f0ee82d082602bdcecbe58f874c074b497f573e
Luca Del Re <delreluca@googlemail.com>
2018-05-13 23:02:43 +0200

    Add charting code using XPlot.Plotly

---
+++ Charts.fs
@@ -0,0 +1,21 @@
+namespace Bascplain
+
+module Charts =
+
+    open Analysis
+    open XPlot.Plotly
+
+    let getArbitraryTimeSeriesChart g ys =
+        let ls = ys |> Seq.map fst
+        let mapSnd f us = us |> Seq.map (fun (x,y) -> x, f y)
+        let ts = ys |> Seq.map snd |> mapSnd g |> Chart.Line |> Chart.WithLabels ls
+
+        ts.GetInlineHtml()
+
+    let getNavChart ys = getArbitraryTimeSeriesChart (fun a -> a.Nav) ys
+
+    let getWeightChart ys = getArbitraryTimeSeriesChart (fun a -> a.Weight * 100m) ys
+
+    let getDailyReturnChart ys = getArbitraryTimeSeriesChart (fun a -> a.PerformanceNow.Return * 100m) ys
+
+    let getPerformanceChart ys = getArbitraryTimeSeriesChart (fun a -> a.PerformanceNow.AccumulatedPerformance * 100m) ys
