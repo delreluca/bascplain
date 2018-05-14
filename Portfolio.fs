6379f189d30ce0acf18989527ef98e49b002356a
Luca Del Re <delreluca@googlemail.com>
2018-05-14 23:13:25 +0200

    Fix missing first flow position

--- Portfolio.fs
+++ Portfolio.fs
@@ -28,4 +28,4 @@ module Portfolio =
         let foldReturns fs (s) = match fs with
                                   | f :: fs' -> { Snapshot = s; ReturnOnNext = 0.0m; } :: { f with ReturnOnNext = s.Quote / f.Snapshot.Quote - 1.0m } :: fs'
                                   | [] -> [{Snapshot = s; ReturnOnNext = 0.0m }]
-        xs |> Seq.fold foldReturns [] |> Seq.rev |> Seq.skip 1
+        xs |> Seq.fold foldReturns [] |> Seq.rev
