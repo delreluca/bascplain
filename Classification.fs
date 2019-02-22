48472b1991807013206c30d87391c897f6e12a15
Luca Del Re <delreluca@googlemail.com>
2019-02-22 08:23:19 +0100

    Add missing ISINs found in my own records

--- Classification.fs
+++ Classification.fs
@@ -71,20 +71,22 @@ module Classification =
                             | "IE0032895942" -> Some { What = FixedIncome Corporate; Where =  Some USA }
                             | "IE00B1FZS350" -> Some { What = RealEstate; Where =  Some DevelopedMarkets }
                             | "IE00B1FZS798" -> Some { What = FixedIncome Sovereign; Where =  Some USA }
+                            | "IE00B1FZSF77" -> Some { What = RealEstate; Where = Some USA } //discovered 18 May 2018
                             | "IE00B2NPKV68" -> Some { What = FixedIncome Sovereign; Where =  Some EmergingMarkets }
                             | "IE00B3B8Q275" -> Some { What = FixedIncome Covered; Where =  Some Eurozone }
                             | "IE00B3F81R35" -> Some { What = FixedIncome Corporate; Where =  Some Eurozone }
                             | "IE00B3XXRP09" -> Some { What = Equity; Where =  Some USA }
                             | "IE00B4WXJJ64" -> Some { What = FixedIncome Sovereign; Where =  Some Eurozone }
+                            | "IE00B52MJY50" -> Some { What = Equity; Where = Some PacificExJapan } //discovered 22 Jun 2018
                             | "IE00B5BMR087" -> Some { What = Equity; Where = Some USA }
+                            | "IE00BJ38QD84" -> Some { What = Equity; Where = Some USA } //Small Cap
                             | "IE00BTJRMP35" -> Some { What = Equity; Where = Some EmergingMarkets }
                             | "LU0136240974" -> Some { What = Equity; Where = Some Japan }
+                            | "LU0290355717" -> Some { What = FixedIncome Sovereign; Where = Some Eurozone } //discovered 4 Jul 2018
                             | "LU0446734526" -> Some { What = Equity; Where = Some PacificExJapan }
                             | "LU0480132876" -> Some { What = Equity; Where = Some EmergingMarkets }
-                            | "LU0839027447" -> Some { What = Equity; Where = Some Japan }
                             | "LU0489337690" -> Some { What = RealEstate; Where = Some Europe } //discovered 18 May 2018
-                            | "IE00B1FZSF77" -> Some { What = RealEstate; Where = Some USA } //discovered 18 May 2018
-                            | "IE00B52MJY50" -> Some { What = Equity; Where = Some PacificExJapan } //discovered 22 Jun 2018
-                            | "LU0290355717" -> Some { What = FixedIncome Sovereign; Where = Some Eurozone } //discovered 4 Jul 2018
+                            | "LU0839027447" -> Some { What = Equity; Where = Some Japan }
+                            | "LU0908500753" -> Some { What = Equity; Where = Some Europe }
                             | "<CASH>" -> Some { What = Cash; Where = None; }
                             | _ -> None
